26b823de6ba07ae2f25b81334396112ac286436e
CatValentine <71935073+CatValentine-Dev@users.noreply.github.com>
2022-11-12 20:04:11 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/components/App';
 import { setConfig } from 'react-hot-loader';
-import './MinecraftPurpleTheme.css';
+import './pterodactylthemes.css';
 
 // Enable language support.
 import './i18n';
